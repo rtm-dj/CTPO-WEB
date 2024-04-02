967f0189f5f2696af7a965d5787396fe04a42264
Artem <artem@yugaev.ru>
2024-04-02 19:36:05 +0300

    lesson 02 march

---
+++ FlowerStore/flowerstore/src/components/Tag.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="tag" :style="{backgroundColor: color}">
+      <p>{{ text }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['text', 'color']
+}
+</script>
+
+<style scoped>
+.tag{
+    width: fit-content;
+    height: 20px;
+    border-radius: 5px;
+    padding: 4px 8px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.tag p{
+    margin: 0;
+    font-size: 0.8rem;
+}
+</style>
