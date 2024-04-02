c00691afcbe26f6042559ef908f6caf0ef0135c0
Artem <artem@yugaev.ru>
2024-04-02 17:57:54 +0300

    created

---
+++ FlowerStore/flowerstore/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+
+import NavBar from '../src/components/NavBar.vue'
+
+export default {
+    components: { NavBar },
+}
+
+</script>
+
+<template>
+  <NavBar/>
+
+  <RouterView />
+</template>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+
+  body{
+    font-family: 'Montserrat';
+  }
+</style>
